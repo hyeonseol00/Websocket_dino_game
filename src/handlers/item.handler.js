88ed2442c86e945f144157b43149aaebadd18939
hyeonseol00 <hyeonseol22@naver.com>
2024-06-11 15:59:06 +0900

    ✨ update : 아이템 획득 시 검증단계 추가

--- src/handlers/item.handler.js
+++ src/handlers/item.handler.js
@@ -1,10 +1,20 @@
 import { getGameAssets } from "../init/assets.js";
 import { getItem, setItem } from "../models/item.model.js";
+import { getStage } from "../models/stage.model.js";
 
 export const getItemHandler = (userId, payload) =>
 {
-	const { items } = getGameAssets();
+	const { items, itemUnlocks } = getGameAssets();
+
 	let currentItems = getItem(userId);
+	currentItems.sort((a, b) => a.timestamp - b.timestamp);
+	const lastItem = currentItems.at(-1);
+
+	let currentStages = getStage(userId);
+	currentStages.sort((a, b) => a.id - b.id);
+	const currentStage = currentStages[currentStages.length - 1];
+
+	const serverTime = Date.now();
 
 	if (currentItems === undefined)
 	{
@@ -23,7 +33,24 @@ export const getItemHandler = (userId, payload) =>
 		return { status: "fail", message: "해당 아이템에 맞는 점수가 아닙니다!" };
 	}
 
-	setItem(userId, payload.itemId);
+	// 획득한 아이템이 해당 스테이지에 등장하는 아이템이 맞는지 검증
+	if (!itemUnlocks.data[currentStage.id - 1001].item_id.includes(payload.itemId))
+	{
+		return { status: "fail", message: "현재 스테이지에 등장하는 아이템이 아닙니다!" };
+	}
+
+	// 아이템을 획득한 시간간격 검증
+	if (lastItem)
+	{
+		const elapsedTime = (serverTime - lastItem.timestamp) / 1000;
+
+		if (elapsedTime <= 2.5)
+		{
+			return { status: "fail", message: "비정상적인 아이템 획득 속도입니다!" };
+		}
+	}
+
+	setItem(userId, payload.itemId, serverTime);
 
 	return { status: "success", message: `${payload.itemId}번 아이템 획득 성공` };
 };
