9f89f0f3381660ceeb71d7ab4ad84857c07637c7
hyeonseol00 <hyeonseol22@naver.com>
2024-06-13 17:30:04 +0900

    ✨ update : 최고기록을 달성한 적이 있는 유저가 접속 시 메시지 출력

--- src/handlers/score.handler.js
+++ src/handlers/score.handler.js
@@ -1,7 +1,9 @@
 import { getHighScore, setHighScore } from "../models/score.model.js";
+import { updateUuid } from "../models/uuid.model.js";
 
 export const updateHighScore = async (uuid, payload) =>
 {
+	updateUuid(uuid);
 	setHighScore(payload.highScore);
 
 	return { broadcast: { status: "success", message: "최고기록이 갱신되었습니다.", highScore: await getHighScore() } };
