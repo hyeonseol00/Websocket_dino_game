764df94585028b18da1dcb3dff70af86371ff6f9
hyeonseol00 <hyeonseol22@naver.com>
2024-06-13 13:06:05 +0900

    ♻️ refactor : 아이템 정보 저장도 redis 사용해서 리팩토링

--- src/models/item.model.js
+++ src/models/item.model.js
@@ -1,22 +1,52 @@
-const items = {};
+import redisCli from "../../redis/redis.client.js";
 
-// 아이템 초기화
-export const createItem = (uuid) =>
+const itemsKey = "items";
+
+// 스테이지 초기화
+export const createItem = async (uuid) =>
 {
+	const itemsJSON = await redisCli.get(itemsKey);
+	const items = await JSON.parse(itemsJSON);
+
+	if (!items)
+	{
+		await redisCli.set(itemsKey, `{ "${uuid}": [] }`);
+		return;
+	}
+
 	items[uuid] = [];
+	await redisCli.set(itemsKey, JSON.stringify(items));
 };
 
-export const getItem = (uuid) =>
+export const getItem = async (uuid) =>
 {
+	const itemsJSON = await redisCli.get(itemsKey);
+	const items = await JSON.parse(itemsJSON);
+
 	return items[uuid];
 };
 
-export const setItem = (uuid, id, timestamp) =>
+export const setItem = async (uuid, id, timestamp) =>
 {
-	return items[uuid].push({ id, timestamp });
+	const itemsJSON = await redisCli.get(itemsKey);
+	const items = await JSON.parse(itemsJSON);
+
+	items[uuid].push({ id, timestamp });
+
+	await redisCli.set(itemsKey, JSON.stringify(items));
 };
 
-export const clearItem = (uuid) =>
+export const clearItem = async (uuid) =>
 {
+	const itemsJSON = await redisCli.get(itemsKey);
+	const items = await JSON.parse(itemsJSON);
+
+	if (!items)
+	{
+		await redisCli.set(itemsKey, `{ "${uuid}": [] }`);
+		return;
+	}
+
 	items[uuid] = [];
+	await redisCli.set(itemsKey, JSON.stringify(items));
 };
