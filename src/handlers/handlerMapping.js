fca052938e032a0998b3737e0572c170d784ac2f
hyeonseol00 <hyeonseol22@naver.com>
2024-06-13 19:30:14 +0900

    🩹 fix : 최고점수 갱신 시점 변경

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,6 +1,5 @@
 import { gameEnd, gameStart } from "./game.handler.js";
 import { getItemHandler } from "./item.handler.js";
-import { updateHighScore } from "./score.handler.js";
 import { moveStageHandler } from "./stage.handler.js";
 
 const handlerMappings = {
@@ -8,7 +7,6 @@ const handlerMappings = {
 	3: gameEnd,
 	11: moveStageHandler,
 	21: getItemHandler,
-	31: updateHighScore
 };
 
 export default handlerMappings;
--- src/handlers/score.handler.js
+++ src/handlers/score.handler.js
@@ -4,7 +4,8 @@ import { updateUuid } from "../models/uuid.model.js";
 export const updateHighScore = async (uuid, payload) =>
 {
 	updateUuid(uuid);
-	setHighScore(payload.highScore);
+	if (+payload.highScore >= await getHighScore())
+		setHighScore(+payload.highScore);
 
 	return { broadcast: { status: "success", message: "최고기록이 갱신되었습니다.", highScore: await getHighScore() } };
 };
